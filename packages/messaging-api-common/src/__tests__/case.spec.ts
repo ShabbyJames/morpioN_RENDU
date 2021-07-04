932280623a50e4fb9f3e69b9d659c315431fa3a8
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2021-07-04 23:01:09 +0000

    Fix stuff.

--- packages/messaging-api-common/src/__tests__/case.spec.ts
+++ packages/messaging-api-common/src/__tests__/case.spec.ts
@@ -39,4 +39,53 @@ it('snakecaseKeysDeep', () => {
 it('camelcase', () => {
   expect(camelcase('my_key')).toEqual('myKey');
   expect(camelcase('has_2fa')).toEqual('has2fa');
-  expect(camelcase('image_1024')).toEqual('image10
+  expect(camelcase('image_1024')).toEqual('image1024');
+});
+
+it('camelcaseKeys', () => {
+  expect(camelcaseKeys({ my_key: 'value' })).toEqual({
+    myKey: 'value',
+  });
+  expect(
+    camelcaseKeys({ my_obj: { my_key: 'value' } }, { deep: false })
+  ).toEqual({
+    myObj: { my_key: 'value' },
+  });
+  expect(
+    camelcaseKeys({ my_obj: { my_key: 'value' } }, { deep: true })
+  ).toEqual({
+    myObj: { myKey: 'value' },
+  });
+});
+
+it('camelcaseKeysDeep', () => {
+  expect(camelcaseKeysDeep({ my_obj: { my_key: 'value' } })).toEqual({
+    myObj: { myKey: 'value' },
+  });
+});
+
+it('pascalcase', () => {
+  expect(pascalcase('myKey')).toEqual('MyKey');
+});
+
+it('pascalcaseKeys', () => {
+  expect(pascalcaseKeys({ myKey: 'value' })).toEqual({
+    MyKey: 'value',
+  });
+  expect(
+    pascalcaseKeys({ myObj: { myKey: 'value' } }, { deep: false })
+  ).toEqual({
+    MyObj: { myKey: 'value' },
+  });
+  expect(pascalcaseKeys({ myObj: { myKey: 'value' } }, { deep: true })).toEqual(
+    {
+      MyObj: { MyKey: 'value' },
+    }
+  );
+});
+
+it('pascalcaseKeysDeep', () => {
+  expect(pascalcaseKeysDeep({ myObj: { myKey: 'value' } })).toEqual({
+    MyObj: { MyKey: 'value' },
+  });
+});
---
+++ packages/messaging-api-common/src/case.ts
@@ -0,0 +1,144 @@
+
+import mapObject from 'map-obj';
+import { camelCase } from 'camel-case';
+import { pascalCase } from 'pascal-case';
+import { snakeCase } from 'snake-case';
+import type {
+  CamelCase,
+  CamelCasedProperties,
+  CamelCasedPropertiesDeep,
+  PascalCase,
+  PascalCasedProperties,
+  PascalCasedPropertiesDeep,
+  SnakeCase,
+  SnakeCasedProperties,
+  SnakeCasedPropertiesDeep,
+} from 'type-fest';
+import type { Options as MapOptions } from 'map-obj';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type PlainObject = Record<string, any>;
+
+function isLastCharNumber(key: string): boolean {
+  return /^\d$/.test(key[key.length - 1]);
+}
+
+function splitLastChar(key: string): string {
+  return `${key.slice(0, key.length - 1)}_${key.slice(
+    key.length - 1,
+    key.length
+  )}`;
+}
+
+/**
+ * Converts a string to snake case.
+ *
+ * @param text - The input string
+ * @returns The converted string
+ *
+ * @example
+ * ```js
+ * snakecase('fooBar');
+ * //=> 'foo_bar'
+ * ```
+ */
+function snakecase<T extends string>(text: T): SnakeCase<T> {
+  const matches = text.match(/\d+/g);
+  if (!matches) {
+    return snakeCase(text) as SnakeCase<T>;
+  }
+
+  let modifiedStr: string = text;
+  for (let i = 0; i < matches.length; i++) {
+    const match = matches[i];
+    const mathIndex = modifiedStr.indexOf(match);
+    modifiedStr = `${modifiedStr.slice(0, mathIndex)}_${modifiedStr.slice(
+      mathIndex,
+      modifiedStr.length
+    )}`;
+  }
+
+  return snakeCase(modifiedStr) as SnakeCase<T>;
+}
+
+/**
+ * Converts object keys to snake case.
+ *
+ * @param obj - The input object
+ * @param options - The options to config this convert function
+ * @returns The converted object
+ *
+ * @example
+ * ```js
+ * snakecaseKeys({ 'fooBar': true });
+ * //=> { 'foo_bar': true }
+ * ```
+ */
+function snakecaseKeys<T extends PlainObject, O extends MapOptions>(
+  obj: T,
+  options?: O
+): O['deep'] extends true
+  ? SnakeCasedPropertiesDeep<T>
+  : SnakeCasedProperties<T> {
+  return mapObject(
+    obj,
+    (key, val) => [snakecase(key as string), val],
+    options
+  ) as O['deep'] extends true
+    ? SnakeCasedPropertiesDeep<T>
+    : SnakeCasedProperties<T>;
+}
+
+/**
+ * Converts object keys to snake case deeply.
+ *
+ * @param obj - The input object
+ * @returns The converted object
+ *
+ * @example
+ * ```js
+ * snakecaseKeysDeep({ 'fooBar': { 'barFoo': true } });
+ * //=> { 'foo_bar': { 'bar_foo': true } }
+ * ```
+ */
+function snakecaseKeysDeep<T extends PlainObject>(
+  obj: T
+): SnakeCasedPropertiesDeep<T> {
+  return snakecaseKeys(obj, { deep: true });
+}
+
+/**
+ * Converts a string to camel case.
+ *
+ * @param text - The input string
+ * @returns The converted string
+ *
+ * @example
+ * ```js
+ * camelcase('foo_bar');
+ * //=> 'fooBar'
+ * ```
+ */
+function camelcase<T extends string>(text: T): CamelCase<T> {
+  const parts = text.split('_');
+  const modifiedStr = parts.reduce((acc, part) => {
+    if (acc === '') return part;
+    if (/^\d+/.test(part)) {
+      return acc + part;
+    }
+    return `${acc}_${part}`;
+  }, '');
+  return camelCase(modifiedStr) as CamelCase<T>;
+}
+
+/**
+ * Converts object keys to camel case.
+ *
+ * @param obj - The input object
+ * @param options - The options to config this convert function
+ * @returns The converted object
+ *
+ * @example
+ * ```js
+ * camelcaseKeys({ 'foo_bar': true });
+ * //=> { 'fooBar': true }
