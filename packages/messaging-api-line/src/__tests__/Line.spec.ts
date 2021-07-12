e8020652ab61871abc5c6a737b9c8e709be44f0b
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2021-07-12 15:49:13 +0000

    subject

--- packages/messaging-api-line/src/__tests__/Line.spec.ts
+++ packages/messaging-api-line/src/__tests__/Line.spec.ts
@@ -1,1 +1,56 @@
-import
+import * as Line from '../Line';
+import * as Types from '../LineTypes';
+
+const quickReply: Types.QuickReply = {
+  items: [
+    {
+      type: 'action',
+      action: {
+        type: 'cameraRoll',
+        label: 'Send photo',
+      },
+    },
+    {
+      type: 'action',
+      action: {
+        type: 'camera',
+        label: 'Open camera',
+      },
+    },
+  ],
+};
+
+describe('#createText', () => {
+  it('should return text message object', () => {
+    expect(Line.createText('t')).toEqual({ type: 'text', text: 't' });
+  });
+
+  it('should work with quickReply', () => {
+    expect(Line.createText('t', { quickReply })).toEqual({
+      type: 'text',
+      text: 't',
+      quickReply,
+    });
+  });
+});
+
+describe('#createImage', () => {
+  it('should return image message object', () => {
+    expect(
+      Line.createImage({
+        originalContentUrl: 'http://example.com/img1.jpg',
+      })
+    ).toEqual({
+      type: 'image',
+      originalContentUrl: 'http://example.com/img1.jpg',
+      previewImageUrl: 'http://example.com/img1.jpg',
+    });
+
+    expect(
+      Line.createImage({
+        originalContentUrl: 'http://example.com/img1.jpg',
+        previewImageUrl: 'http://example.com/img2.jpg',
+      })
+    ).toEqual({
+      type: 'image',
+      originalContentUrl: 'http://example.com/i
