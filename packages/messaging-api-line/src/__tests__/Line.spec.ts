ee4e1bb8d97d1208007e790e3a2fb3ae0e927239
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2021-07-15 15:29:04 +0000

    [tracker] Corrections

--- packages/messaging-api-line/src/__tests__/Line.spec.ts
+++ packages/messaging-api-line/src/__tests__/Line.spec.ts
@@ -386,4 +386,54 @@ describe('#createButtonTemplate', () => {
           },
           {
             type: 'uri',
-            label
+            label: 'View detail',
+            uri: 'http://example.com/page/123',
+          },
+        ],
+      })
+    ).toEqual({
+      type: 'template',
+      altText: 'this is a buttons template',
+      template: {
+        type: 'buttons',
+        thumbnailImageUrl: 'https://example.com/bot/images/image.jpg',
+        title: 'Menu',
+        text: 'Please select',
+        defaultAction: {
+          type: 'uri',
+          label: 'View detail',
+          uri: 'http://example.com/page/123',
+        },
+        actions: [
+          {
+            type: 'postback',
+            label: 'Buy',
+            data: 'action=buy&itemid=123',
+          },
+          {
+            type: 'postback',
+            label: 'Add to cart',
+            data: 'action=add&itemid=123',
+          },
+          {
+            type: 'uri',
+            label: 'View detail',
+            uri: 'http://example.com/page/123',
+          },
+        ],
+      },
+    });
+  });
+
+  it('should work with quickReply', () => {
+    expect(
+      Line.createButtonTemplate(
+        'this is a buttons template',
+        {
+          thumbnailImageUrl: 'https://example.com/bot/images/image.jpg',
+          title: 'Menu',
+          text: 'Please select',
+          actions: [
+            {
+              type: 'postback',
+              label
