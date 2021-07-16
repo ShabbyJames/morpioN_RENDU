1ab965754ee9a177040e1b400df99c5669ebc904
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2021-07-16 06:30:18 +0000

    unused parameter

--- packages/messaging-api-line/src/__tests__/Line.spec.ts
+++ packages/messaging-api-line/src/__tests__/Line.spec.ts
@@ -436,4 +436,54 @@ describe('#createButtonTemplate', () => {
           actions: [
             {
               type: 'postback',
-              label
+              label: 'Buy',
+              data: 'action=buy&itemid=123',
+            },
+            {
+              type: 'postback',
+              label: 'Add to cart',
+              data: 'action=add&itemid=123',
+            },
+            {
+              type: 'uri',
+              label: 'View detail',
+              uri: 'http://example.com/page/123',
+            },
+          ],
+        },
+        { quickReply }
+      )
+    ).toEqual({
+      type: 'template',
+      altText: 'this is a buttons template',
+      template: {
+        type: 'buttons',
+        thumbnailImageUrl: 'https://example.com/bot/images/image.jpg',
+        title: 'Menu',
+        text: 'Please select',
+        actions: [
+          {
+            type: 'postback',
+            label: 'Buy',
+            data: 'action=buy&itemid=123',
+          },
+          {
+            type: 'postback',
+            label: 'Add to cart',
+            data: 'action=add&itemid=123',
+          },
+          {
+            type: 'uri',
+            label: 'View detail',
+            uri: 'http://example.com/page/123',
+          },
+        ],
+      },
+      quickReply,
+    });
+  });
+
+  it('should support createButtonsTemplate alias', () => {
+    expect(
+      Line.createButtonsTemplate('this is a buttons template', {
+        t
