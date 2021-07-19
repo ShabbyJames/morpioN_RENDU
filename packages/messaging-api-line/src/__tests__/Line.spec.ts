6a315c1c38e26d08be46f307525e4a9d49a57010
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2021-07-19 09:23:02 +0000

    fix validation errors

--- packages/messaging-api-line/src/__tests__/Line.spec.ts
+++ packages/messaging-api-line/src/__tests__/Line.spec.ts
@@ -838,4 +838,52 @@ describe('#createCarouselTemplate', () => {
 describe('#createImageCarouselTemplate', () => {
   it('should return image carousel template message object', () => {
     expect(
-      Line.createImageCarouselTemplate('this is
+      Line.createImageCarouselTemplate('this is a image carousel template', [
+        {
+          imageUrl: 'https://example.com/bot/images/item1.jpg',
+          action: {
+            type: 'postback',
+            label: 'Buy',
+            data: 'action=buy&itemid=111',
+          },
+        },
+        {
+          imageUrl: 'https://example.com/bot/images/item2.jpg',
+          action: {
+            type: 'message',
+            label: 'Yes',
+            text: 'yes',
+          },
+        },
+        {
+          imageUrl: 'https://example.com/bot/images/item3.jpg',
+          action: {
+            type: 'uri',
+            label: 'View detail',
+            uri: 'http://example.com/page/222',
+          },
+        },
+      ])
+    ).toEqual({
+      type: 'template',
+      altText: 'this is a image carousel template',
+      template: {
+        type: 'image_carousel',
+        columns: [
+          {
+            imageUrl: 'https://example.com/bot/images/item1.jpg',
+            action: {
+              type: 'postback',
+              label: 'Buy',
+              data: 'action=buy&itemid=111',
+            },
+          },
+          {
+            imageUrl: 'https://example.com/bot/images/item2.jpg',
+            action: {
+              type: 'message',
+              label: 'Yes',
+              text: 'yes',
+            },
+          },
+         
