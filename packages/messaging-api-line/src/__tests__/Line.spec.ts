25ef1f3be3f83ca27a748da83c5299416c2426f2
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2021-07-19 06:40:16 +0000

    minor javadoc

--- packages/messaging-api-line/src/__tests__/Line.spec.ts
+++ packages/messaging-api-line/src/__tests__/Line.spec.ts
@@ -791,4 +791,51 @@ describe('#createCarouselTemplate', () => {
             actions: [
               {
                 type: 'postback',
-                
+                label: 'Buy',
+                data: 'action=buy&itemid=111',
+              },
+              {
+                type: 'postback',
+                label: 'Add to cart',
+                data: 'action=add&itemid=111',
+              },
+              {
+                type: 'uri',
+                label: 'View detail',
+                uri: 'http://example.com/page/111',
+              },
+            ],
+          },
+          {
+            thumbnailImageUrl: 'https://example.com/bot/images/item2.jpg',
+            title: 'this is menu',
+            text: 'description',
+            actions: [
+              {
+                type: 'postback',
+                label: 'Buy',
+                data: 'action=buy&itemid=222',
+              },
+              {
+                type: 'postback',
+                label: 'Add to cart',
+                data: 'action=add&itemid=222',
+              },
+              {
+                type: 'uri',
+                label: 'View detail',
+                uri: 'http://example.com/page/222',
+              },
+            ],
+          },
+        ],
+      },
+      quickReply,
+    });
+  });
+});
+
+describe('#createImageCarouselTemplate', () => {
+  it('should return image carousel template message object', () => {
+    expect(
+      Line.createImageCarouselTemplate('this is
