86d0a3dd43582cec717807cbb9b3f2c4e5bd0670
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2021-07-20 13:05:23 +0000

    3.0.1 release

--- packages/messaging-api-line/src/__tests__/Line.spec.ts
+++ packages/messaging-api-line/src/__tests__/Line.spec.ts
@@ -987,4 +987,64 @@ describe('#createFlex', () => {
           type: 'image',
           url: 'https://example.com/flex/images/image.jpg',
         },
-        bo
+        body: {
+          type: 'box',
+          layout: 'vertical',
+          contents: [
+            {
+              type: 'text',
+              text: 'Body text',
+            },
+          ],
+        },
+        footer: {
+          type: 'box',
+          layout: 'vertical',
+          contents: [
+            {
+              type: 'text',
+              text: 'Footer text',
+            },
+          ],
+        },
+        styles: {
+          header: {
+            separator: true,
+          },
+        },
+      })
+    ).toEqual({
+      type: 'flex',
+      altText: 'this is a flex message',
+      contents: {
+        type: 'bubble',
+        header: {
+          type: 'box',
+          layout: 'vertical',
+          contents: [
+            {
+              type: 'text',
+              text: 'Header text',
+            },
+          ],
+        },
+        hero: {
+          type: 'image',
+          url: 'https://example.com/flex/images/image.jpg',
+        },
+        body: {
+          type: 'box',
+          layout: 'vertical',
+          contents: [
+            {
+              type: 'text',
+              text: 'Body text',
+            },
+          ],
+        },
+        footer: {
+          type: 'box',
+          layout: 'vertical',
+          contents: [
+            {
+            
