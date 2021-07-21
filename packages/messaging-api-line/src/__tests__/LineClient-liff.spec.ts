587c8aa8de84030a2b301a5c4fb5bb7d9acf1250
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2021-07-21 14:23:43 +0000

    Add screenshots to README

--- packages/messaging-api-line/src/__tests__/LineClient-liff.spec.ts
+++ packages/messaging-api-line/src/__tests__/LineClient-liff.spec.ts
@@ -20,4 +20,56 @@ const createMock = (): {
   });
   const mock = new MockAdapter(client.axios);
   const headers = {
-    Accept: 'appli
+    Accept: 'application/json, text/plain, */*',
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${ACCESS_TOKEN}`,
+  };
+  return { client, mock, headers };
+};
+
+describe('LINE Front-end Framework', () => {
+  describe('#getLiffAppList', () => {
+    it('should call api', async () => {
+      expect.assertions(4);
+
+      const { client, mock, headers } = createMock();
+
+      const reply = {
+        apps: [
+          {
+            liffId: 'liff-12345',
+            view: {
+              type: 'full',
+              url: 'https://example.com/myservice',
+            },
+          },
+          {
+            liffId: 'liff-67890',
+            view: {
+              type: 'tall',
+              url: 'https://example.com/myservice2',
+            },
+          },
+        ],
+      };
+
+      mock.onGet().reply((config) => {
+        expect(config.url).toEqual('/liff/v1/apps');
+        expect(config.data).toEqual(undefined);
+        expect(config.headers).toEqual(headers);
+        return [200, reply];
+      });
+
+      const res = await client.getLiffAppList();
+
+      expect(res).toEqual([
+        {
+          liffId: 'liff-12345',
+          view: {
+            type: 'full',
+            url: 'https://example.com/myservice',
+          },
+        },
+        {
+          liffId: 'liff-67890',
+      
