c1ebc7f674a67bfe3bb1b992a48021f403effe52
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2021-07-24 12:55:00 +0000

    various stuff

--- packages/messaging-api-line/src/__tests__/LineClient-menu.spec.ts
+++ packages/messaging-api-line/src/__tests__/LineClient-menu.spec.ts
@@ -14,4 +14,50 @@ const createMock = (): {
   dataMock: MockAdapter;
   headers: {
     Accept: string;
-    'C
+    'Content-Type': string;
+    Authorization: string;
+  };
+} => {
+  const client = new LineClient({
+    accessToken: ACCESS_TOKEN,
+    channelSecret: CHANNEL_SECRET,
+  });
+  const mock = new MockAdapter(client.axios);
+  const dataMock = new MockAdapter(client.dataAxios);
+  const headers = {
+    Accept: 'application/json, text/plain, */*',
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${ACCESS_TOKEN}`,
+  };
+  return { client, mock, dataMock, headers };
+};
+
+describe('Rich Menu', () => {
+  describe('#getRichMenuList', () => {
+    it('should call api', async () => {
+      expect.assertions(4);
+
+      const { client, mock, headers } = createMock();
+
+      const reply = {
+        richmenus: [
+          {
+            richMenuId: 'richmenu-8dfdfc571eca39c0ffcd1f799519c5b5',
+            size: {
+              width: 2500,
+              height: 1686,
+            },
+            selected: false,
+            name: 'Nice richmenu',
+            chatBarText: 'Tap here',
+            areas: [
+              {
+                bounds: {
+                  x: 0,
+                  y: 0,
+                  width: 2500,
+                  height: 1686,
+                },
+                action: {
+                  type: 'postback',
+                  data: 'act
