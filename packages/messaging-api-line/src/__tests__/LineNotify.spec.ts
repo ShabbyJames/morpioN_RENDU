d31a35fade642cd6ef484172b7f133c4fca4b99a
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2021-08-01 10:55:45 +0000

    5.1.6

--- packages/messaging-api-line/src/__tests__/LineNotify.spec.ts
+++ packages/messaging-api-line/src/__tests__/LineNotify.spec.ts
@@ -5,4 +5,61 @@ import MockAdapter from 'axios-mock-adapter';
 import LineNotify from '../LineNotify';
 
 const CLIENT_ID = 'client-id';
-const CLIENT_SECRET = 'cli
+const CLIENT_SECRET = 'client-secret';
+const REDIRECT_URI = 'https://example.com/callback';
+
+const createMock = (): {
+  client: LineNotify;
+  mock: MockAdapter;
+  apiMock: MockAdapter;
+} => {
+  const client = new LineNotify({
+    clientId: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    redirectUri: REDIRECT_URI,
+  });
+  const mock = new MockAdapter(client.axios);
+  const apiMock = new MockAdapter(client.apiAxios);
+  return { client, mock, apiMock };
+};
+
+describe('connect', () => {
+  let axios;
+  let _create;
+  beforeEach(() => {
+    axios = require('axios');
+    _create = axios.create;
+  });
+
+  afterEach(() => {
+    axios.create = _create;
+  });
+
+  it('create axios with LINE Notify API', () => {
+    axios.create = jest.fn();
+    LineNotify.connect({
+      clientId: CLIENT_ID,
+      clientSecret: CLIENT_SECRET,
+      redirectUri: REDIRECT_URI,
+    });
+
+    expect(axios.create).toBeCalledWith({
+      baseURL: 'https://notify-bot.line.me/',
+    });
+
+    expect(axios.create).toBeCalledWith({
+      baseURL: 'https://notify-api.line.me/',
+    });
+  });
+});
+
+describe('constructor', () => {
+  let axios;
+  let _create;
+  beforeEach(() => {
+    axios = require('axios');
+    _create = axios.create;
+  });
+
+  afterEach(() => {
+    
