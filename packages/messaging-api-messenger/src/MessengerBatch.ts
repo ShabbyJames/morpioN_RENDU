f980bf8227fdcc91d4113ae1d27cfc30047653fb
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2021-08-03 22:09:02 +0000

    update gulp

--- packages/messaging-api-messenger/src/MessengerBatch.ts
+++ packages/messaging-api-messenger/src/MessengerBatch.ts
@@ -4,4 +4,50 @@ import pick from 'lodash/pick';
 import * as Messenger from './Messenger';
 import * as MessengerTypes from './MessengerTypes';
 
-function omitUndefinedFields(obj = {}): 
+function omitUndefinedFields(obj = {}): object {
+  return JSON.parse(JSON.stringify(obj));
+}
+
+function pickBatchOptions<T extends MessengerTypes.BatchRequestOptions>(
+  options: T
+): Pick<T, 'name' | 'dependsOn' | 'omitResponseOnSuccess'> {
+  return pick(options, ['name', 'dependsOn', 'omitResponseOnSuccess']);
+}
+
+function omitBatchOptions<T extends MessengerTypes.BatchRequestOptions>(
+  options: T
+): Omit<T, 'name' | 'dependsOn' | 'omitResponseOnSuccess'> {
+  return omit(options, ['name', 'dependsOn', 'omitResponseOnSuccess']);
+}
+
+export function sendRequest(
+  body: object,
+  options?: MessengerTypes.BatchRequestOptions
+): MessengerTypes.BatchItem {
+  return {
+    method: 'POST',
+    relativeUrl: 'me/messages',
+    body,
+    ...options,
+  };
+}
+
+export function sendMessage(
+  psidOrRecipient: MessengerTypes.PsidOrRecipient,
+  msg: MessengerTypes.Message,
+  options: MessengerTypes.SendOption & MessengerTypes.BatchRequestOptions = {}
+): MessengerTypes.BatchItem {
+  const recipient =
+    typeof psidOrRecipient === 'string'
+      ? {
+          id: psidOrRecipient,
+        }
+      : psidOrRecipient;
+  let messagingType = 'UPDATE';
+  if (options.messagingType) {
+    messagingType = options.messagingType;
+  } else if (options.tag) {
+    messagingType = 'MESSAGE_TAG';
+  }
+
+  const batchRequestO
