69ba41adc1e1533b601d30a6f319ad3854349774
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2021-08-12 06:37:17 +0000

    Version 2.6.2

--- packages/messaging-api-messenger/src/MessengerTypes.ts
+++ packages/messaging-api-messenger/src/MessengerTypes.ts
@@ -35,4 +35,56 @@ export type RecipientWithUserRef = {
 };
 
 /**
- * Used for Private Replies to reference the 
+ * Used for Private Replies to reference the visitor post to reply to.
+ */
+export type RecipientWithPostId = {
+  postId: string;
+};
+
+/**
+ * Used for Private Replies to reference the post comment to reply to.
+ */
+export type RecipientWithCommentId = {
+  commentId: string;
+};
+
+/**
+ * Used for the Messenger Platform's One-Time Notification API.
+ */
+export type RecipientWithOneTimeNotifToken = {
+  oneTimeNotifToken: string;
+};
+
+/**
+ * Description of the message recipient. All requests must include one to identify the recipient.
+ */
+export type Recipient =
+  | RecipientWithID
+  | RecipientWithPhoneNumber
+  | RecipientWithUserRef
+  | RecipientWithPostId
+  | RecipientWithCommentId
+  | RecipientWithOneTimeNotifToken;
+
+/**
+ * Description of the message recipient. If a string is provided, it will be recognized as a psid.
+ */
+export type PsidOrRecipient = string | Recipient;
+
+export type UrlMediaAttachmentPayload = {
+  url: string;
+  isReusable?: boolean;
+};
+
+export type AttachmentIdAttachmentPayload = {
+  attachmentId: string;
+};
+
+export type MediaAttachmentPayload =
+  | UrlMediaAttachmentPayload
+  | AttachmentIdAttachmentPayload;
+
+export type MediaAttachmentType = 'audio' | 'video' | 'image' | 'file';
+
+export type FileDataAttachmentPayload = {
+  isReusable
