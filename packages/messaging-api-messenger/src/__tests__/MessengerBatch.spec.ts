4151ada87450e519aa1d987730c7e52daa8fe719
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2021-08-14 15:44:49 +0000

    common update

--- packages/messaging-api-messenger/src/__tests__/MessengerBatch.spec.ts
+++ packages/messaging-api-messenger/src/__tests__/MessengerBatch.spec.ts
@@ -34,4 +34,65 @@ describe('sendRequest', () => {
     expect(
       MessengerBatch.sendRequest(
         {
-          messagingType:
+          messagingType: 'UPDATE',
+          message: {
+            text: 'Hello',
+          },
+          recipient: {
+            id: RECIPIENT_ID,
+          },
+        },
+        {
+          name: 'second',
+          dependsOn: 'first',
+          omitResponseOnSuccess: false,
+        }
+      )
+    ).toEqual({
+      method: 'POST',
+      relativeUrl: 'me/messages',
+      body: {
+        messagingType: 'UPDATE',
+        message: {
+          text: 'Hello',
+        },
+        recipient: {
+          id: RECIPIENT_ID,
+        },
+      },
+      name: 'second',
+      dependsOn: 'first',
+      omitResponseOnSuccess: false,
+    });
+  });
+});
+
+describe('sendMessage', () => {
+  it('should create send text request', () => {
+    expect(MessengerBatch.sendMessage(RECIPIENT_ID, { text: 'Hello' })).toEqual(
+      {
+        method: 'POST',
+        relativeUrl: 'me/messages',
+        body: {
+          messagingType: 'UPDATE',
+          message: {
+            text: 'Hello',
+          },
+          recipient: {
+            id: RECIPIENT_ID,
+          },
+        },
+      }
+    );
+  });
+
+  it('should create send text with RESPONSE type', () => {
+    expect(
+      MessengerBatch.sendMessage(
+        RECIPIENT_ID,
+        { text: 'Hello' },
+        { messagingType: 'RESPONSE' }
+      )
+    ).toEqual({
+      method: 'POST',
+      relativeUrl: 'me/messages
