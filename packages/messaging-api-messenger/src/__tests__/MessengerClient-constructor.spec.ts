9ce2b69332ec19dc0323f1a993c8fb73089de1fb
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2021-08-27 06:09:15 +0000

    fixup tests

--- packages/messaging-api-messenger/src/__tests__/MessengerClient-constructor.spec.ts
+++ packages/messaging-api-messenger/src/__tests__/MessengerClient-constructor.spec.ts
@@ -2,4 +2,53 @@ import MockAdapter from 'axios-mock-adapter';
 
 import { MessengerBatch, MessengerClient } from '..';
 
-const ACCESS_T
+const ACCESS_TOKEN = 'foo_token';
+const APP_SECRET = 'shhhhh!is.my.secret';
+
+let axios;
+let _create;
+beforeEach(() => {
+  axios = require('axios');
+  _create = axios.create;
+});
+
+afterEach(() => {
+  axios.create = _create;
+});
+
+describe('connect', () => {
+  describe('create axios with default graphAPI version', () => {
+    it('with config', () => {
+      axios.create = jest.fn().mockReturnValue({
+        interceptors: {
+          request: {
+            use: jest.fn(),
+          },
+        },
+      });
+      MessengerClient.connect({ accessToken: ACCESS_TOKEN });
+
+      expect(axios.create).toBeCalledWith(
+        expect.objectContaining({
+          baseURL: 'https://graph.facebook.com/v6.0/',
+          headers: { 'Content-Type': 'application/json' },
+        })
+      );
+    });
+  });
+
+  describe('create axios with custom graphAPI version', () => {
+    it('with config', () => {
+      axios.create = jest.fn().mockReturnValue({
+        interceptors: {
+          request: {
+            use: jest.fn(),
+          },
+        },
+      });
+      MessengerClient.connect({ accessToken: ACCESS_TOKEN, version: '2.6' });
+
+      expect(axios.create).toBeCalledWith(
+        expect.objectContaining({
+          baseURL: 'https://graph.facebook.com/v2.6/',
+          headers: { 'Content-Type': 'application/json
