e659bc7eb53e7b012990934d001dc21a99750dcf
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2021-08-27 08:37:55 +0000

    Triangle

--- packages/messaging-api-messenger/src/__tests__/MessengerClient-constructor.spec.ts
+++ packages/messaging-api-messenger/src/__tests__/MessengerClient-constructor.spec.ts
@@ -51,4 +51,57 @@ describe('connect', () => {
       expect(axios.create).toBeCalledWith(
         expect.objectContaining({
           baseURL: 'https://graph.facebook.com/v2.6/',
-          headers: { 'Content-Type': 'application/json
+          headers: { 'Content-Type': 'application/json' },
+        })
+      );
+    });
+  });
+
+  it('support origin', () => {
+    axios.create = jest.fn().mockReturnValue({
+      interceptors: {
+        request: {
+          use: jest.fn(),
+        },
+      },
+    });
+    MessengerClient.connect({
+      accessToken: ACCESS_TOKEN,
+      origin: 'https://mydummytestserver.com',
+    });
+
+    expect(axios.create).toBeCalledWith(
+      expect.objectContaining({
+        baseURL: 'https://mydummytestserver.com/v6.0/',
+        headers: { 'Content-Type': 'application/json' },
+      })
+    );
+  });
+});
+
+describe('constructor', () => {
+  describe('create axios with default graphAPI version', () => {
+    it('with config', () => {
+      axios.create = jest.fn().mockReturnValue({
+        interceptors: {
+          request: {
+            use: jest.fn(),
+          },
+        },
+      });
+      new MessengerClient({ accessToken: ACCESS_TOKEN }); // eslint-disable-line no-new
+
+      expect(axios.create).toBeCalledWith(
+        expect.objectContaining({
+          baseURL: 'https://graph.facebook.com/v6.0/',
+          headers: { 'Content-Type': 'application/json' },
+        })
+      );
+    });
+  });
+
+  describe('create axios with custom graphAPI version', () => {
+    it('with config', () => {
+      axios.create = jest.fn().mockReturnValue({
+        interceptors: {
+    
