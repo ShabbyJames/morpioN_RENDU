170c7ba14bdbafcc0e5d25ce054eaafaadb294f8
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2021-09-05 16:25:48 +0000

    #Linux#  Add of_node_put to avoid memory leak

--- packages/messaging-api-messenger/src/__tests__/MessengerClient-send.spec.ts
+++ packages/messaging-api-messenger/src/__tests__/MessengerClient-send.spec.ts
@@ -451,4 +451,63 @@ describe('send api', () => {
       expect(url).toEqual(`/me/messages?access_token=${ACCESS_TOKEN}`);
       expect(JSON.parse(data)).toEqual({
         messaging_type: 'UPDATE',
-      
+        recipient: {
+          id: USER_ID,
+        },
+        message: {
+          text: 'Hello!',
+        },
+      });
+
+      expect(res).toEqual({
+        recipientId: USER_ID,
+        messageId: 'mid.1489394984387:3dd22de509',
+      });
+    });
+
+    it('should not attach non-array quick_replies to message', async () => {
+      const { client, mock } = createMock();
+
+      const reply = {
+        recipient_id: USER_ID,
+        message_id: 'mid.1489394984387:3dd22de509',
+      };
+
+      let url;
+      let data;
+      mock.onPost().reply((config) => {
+        url = config.url;
+        data = config.data;
+        return [200, reply];
+      });
+
+      const res = await client.sendMessage(
+        USER_ID,
+        {
+          text: 'Hello!',
+        },
+        {
+          // @ts-expect-error
+          quickReplies: {},
+        }
+      );
+
+      expect(url).toEqual(`/me/messages?access_token=${ACCESS_TOKEN}`);
+      expect(JSON.parse(data)).toEqual({
+        messaging_type: 'UPDATE',
+        recipient: {
+          id: USER_ID,
+        },
+        message: {
+          text: 'Hello!',
+        },
+      });
+
+      expect(res).toEqual({
+        recipientId: USER_ID,
+        messageId: 'mid.1489394984387:3dd22de509',
+      });
+    });
+  });
+
+  describe('#sendAtt
