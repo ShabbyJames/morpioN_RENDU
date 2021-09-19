6db7e775191745a7bf5421a122f2c0f9643186c6
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2021-09-19 17:34:00 +0000

    :pencil2:

--- packages/messaging-api-messenger/src/__tests__/MessengerClient-send.spec.ts
+++ packages/messaging-api-messenger/src/__tests__/MessengerClient-send.spec.ts
@@ -667,4 +667,60 @@ describe('send api', () => {
               url: 'https://example.com/audio.mp3',
             },
           },
-   
+        },
+      });
+
+      expect(res).toEqual({
+        recipientId: USER_ID,
+        messageId: 'mid.1489394984387:3dd22de509',
+      });
+    });
+
+    it('can call api with audio attachment_id', async () => {
+      const { client, mock } = createMock();
+
+      const reply = {
+        recipient_id: USER_ID,
+        message_id: 'mid.1489394984387:3dd22de509',
+      };
+
+      let url;
+      let data;
+      mock.onPost().reply((config) => {
+        url = config.url;
+        data = config.data;
+        return [200, reply];
+      });
+
+      const res = await client.sendAudio(USER_ID, {
+        attachmentId: '5566',
+      });
+
+      expect(url).toEqual(`/me/messages?access_token=${ACCESS_TOKEN}`);
+      expect(JSON.parse(data)).toEqual({
+        messaging_type: 'UPDATE',
+        recipient: {
+          id: USER_ID,
+        },
+        message: {
+          attachment: {
+            type: 'audio',
+            payload: {
+              attachment_id: '5566',
+            },
+          },
+        },
+      });
+
+      expect(res).toEqual({
+        recipientId: USER_ID,
+        messageId: 'mid.1489394984387:3dd22de509',
+      });
+    });
+
+    it('can call api with file stream', async () => {
+      const { client, mock } = createMock();
+
+      const reply = {
+        recipient_id: USER_ID,
+       
