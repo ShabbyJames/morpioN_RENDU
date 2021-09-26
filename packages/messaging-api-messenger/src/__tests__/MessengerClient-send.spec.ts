fe564128b7f6fb3aaee612f8b2b924054ed29444
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2021-09-26 18:07:57 +0000

    Update debian7.sh

--- packages/messaging-api-messenger/src/__tests__/MessengerClient-send.spec.ts
+++ packages/messaging-api-messenger/src/__tests__/MessengerClient-send.spec.ts
@@ -776,4 +776,60 @@ describe('send api', () => {
         },
         message: {
           attachment: {
-  
+            type: 'image',
+            payload: {
+              url: 'https://example.com/pic.png',
+            },
+          },
+        },
+      });
+
+      expect(res).toEqual({
+        recipientId: USER_ID,
+        messageId: 'mid.1489394984387:3dd22de509',
+      });
+    });
+
+    it('can call api with image attachment_id', async () => {
+      const { client, mock } = createMock();
+
+      const reply = {
+        recipient_id: USER_ID,
+        message_id: 'mid.1489394984387:3dd22de509',
+      };
+
+      let url;
+      let data;
+      mock.onPost().reply((config) => {
+        url = config.url;
+        data = config.data;
+        return [200, reply];
+      });
+
+      const res = await client.sendImage(USER_ID, {
+        attachmentId: '5566',
+      });
+
+      expect(url).toEqual(`/me/messages?access_token=${ACCESS_TOKEN}`);
+      expect(JSON.parse(data)).toEqual({
+        messaging_type: 'UPDATE',
+        recipient: {
+          id: USER_ID,
+        },
+        message: {
+          attachment: {
+            type: 'image',
+            payload: {
+              attachment_id: '5566',
+            },
+          },
+        },
+      });
+
+      expect(res).toEqual({
+        recipientId: USER_ID,
+        messageId: 'mid.1489394984387:3dd22de509',
+      });
+    });
+
+    it('can call api with file stream
