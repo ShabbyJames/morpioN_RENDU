213c1bc062e073daf365e44c0c3f3caf2f44c22b
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2021-10-10 21:38:58 +0000

    mwifiex: Add missing endian conversion.

--- packages/messaging-api-messenger/src/__tests__/MessengerClient-send.spec.ts
+++ packages/messaging-api-messenger/src/__tests__/MessengerClient-send.spec.ts
@@ -1143,3 +1143,53 @@ describe('send api', () => {
           },
         },
       ]);
+    });
+
+    it('support the includeHeaders option', async () => {
+      const { client, mock } = createMock();
+
+      const reply = [
+        {
+          code: 200,
+          body: '{"recipient_id":"1QAZ2WSX","message_id":"mid.1489394984387:3dd22de509"}',
+        },
+      ];
+
+      const batch = [MessengerBatch.sendText(USER_ID, 'Hello')];
+
+      let url;
+      let data;
+      mock.onPost().reply((config) => {
+        url = config.url;
+        data = config.data;
+        return [200, reply];
+      });
+
+      const res = await client.sendBatch(batch, { includeHeaders: false });
+
+      expect(url).toEqual('/');
+      expect(JSON.parse(data)).toEqual({
+        access_token: ACCESS_TOKEN,
+        include_headers: false,
+        batch: [
+          {
+            method: 'POST',
+            relative_url: 'me/messages',
+            body: `messaging_type=UPDATE&recipient=%7B%22id%22%3A%22${USER_ID}%22%7D&message=%7B%22text%22%3A%22Hello%22%7D`,
+          },
+        ],
+      });
+
+      expect(res).toEqual([
+        {
+          code: 200,
+          body: {
+            recipientId: USER_ID,
+            messageId: 'mid.1489394984387:3dd22de509',
+          },
+        },
+      ]);
+    });
+
+    it('should get correct data according to responseAccessPath', async () => {
+  
