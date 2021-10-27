f64b2b1ba4d4991dc359550c5f048af415ecab59
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2021-10-27 20:28:01 +0000

    batman-adv: fix skb->data assignment

--- packages/messaging-api-slack/src/SlackTypes.ts
+++ packages/messaging-api-slack/src/SlackTypes.ts
@@ -13,4 +13,65 @@ export type Attachment = {
   fields?: {
     title: string;
     value: string;
-    sh
+    short: boolean;
+  }[];
+  imageUrl?: string;
+  thumbUrl?: string;
+  footer?: string;
+  footerIcon?: string;
+  callbackId?: string;
+  attachmentType?: string;
+  actions: {
+    name?: string;
+    text?: string;
+    type?: string;
+    value?: string;
+    style?: string;
+    options?: { text: string; value: string }[];
+    confirm?: {
+      title?: string;
+      text?: string;
+      okText?: string;
+      dismissText?: string;
+    };
+  }[];
+  ts?: number;
+};
+
+export type Message = {
+  text?: string;
+  attachments?: Attachment[] | string;
+  blocks?: MessageBlock[] | string;
+};
+
+// Block Kit
+
+// Composition Objects
+// https://api.slack.com/reference/block-kit/composition-objects
+
+export type CompositionObject = TextObject | ConfirmObject | OptionObject;
+
+export type TextObject = PlainTextObject | MrkdwnObject;
+
+export type PlainTextObject = {
+  type: 'plain_text';
+  text: string;
+  emoji?: boolean;
+};
+
+export type MrkdwnObject = {
+  type: 'mrkdwn';
+  text: string;
+  verbatim?: boolean;
+};
+
+// https://api.slack.com/reference/block-kit/composition-objects#confirm
+export type ConfirmObject = {
+  title: PlainTextObject;
+  text: TextObject;
+  confirm: PlainTextObject;
+  deny: PlainTextObject;
+};
+
+// https://api.slack.com/reference/block-kit/composition-objects#option
+expo
