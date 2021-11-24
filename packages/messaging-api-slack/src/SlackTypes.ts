54adb4cb79b7ddef0f278f32fc00938f3b7a5f66
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2021-11-24 09:04:52 +0000

    merge with main git using update_weblate_git.py.

--- packages/messaging-api-slack/src/SlackTypes.ts
+++ packages/messaging-api-slack/src/SlackTypes.ts
@@ -702,4 +702,52 @@ export type ScheduleMessageOptions = Message & {
    */
   parse?: 'none' | 'full';
   /**
-   * Used in conjunction with thread_ts and indicates whether reply should be made visible to everyone in the channel or conversati
+   * Used in conjunction with thread_ts and indicates whether reply should be made visible to everyone in the channel or conversation. Defaults to false.
+   */
+  replyBroadcast?: boolean;
+  /**
+   * Provide another message's ts value to make this message a reply. Avoid using a reply's ts value; use its parent instead.
+   */
+  threadTs?: string;
+  /**
+   * Pass true to enable unfurling of primarily text-based content.
+   */
+  unfurlLinks?: boolean;
+  /**
+   * Pass false to disable unfurling of media content.
+   */
+  unfurlMedia?: boolean;
+  /**
+   * Unix EPOCH timestamp of time in future to send the message.
+   */
+  postAt?: string;
+};
+
+// chat.scheduledMessages.list
+// https://api.slack.com/methods/chat.scheduledMessages.list
+export type GetScheduledMessagesOptions = {
+  /**
+   * The channel of the scheduled messages
+   */
+  channel?: string;
+  /**
+   * For pagination purposes, this is the `cursor` value returned from a previous call to `chat.scheduledmessages.list` indicating where you want to start this call from.
+   */
+  cursor?: string;
+  /**
+   * A UNIX timestamp of the latest value in the time range
+   */
+  latest?: string;
+  /**
+   * Maximum number of original entries to return.
+   */
+  limit?: number;
+  /**
+   * A UNIX timestamp of the oldest value in the time range
+   */
+  oldest?: string;
+};
+
+// conversations.members
+// https://api.slack.com/methods/conversations.members
+export type Conver
