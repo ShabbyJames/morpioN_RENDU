d3f07cb87f90d5ad6df8da41246094bc5f05b6bb
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2021-12-05 19:06:34 +0000

    staging: comedi: fix a race between do_cmd_ioctl() and read/write

--- packages/messaging-api-slack/src/SlackWebhookClient.ts
+++ packages/messaging-api-slack/src/SlackWebhookClient.ts
@@ -38,4 +38,61 @@ export default class SlackWebhookClient {
 
   constructor(config: ClientConfig) {
     invariant(
-      typeof config !== 'st
+      typeof config !== 'string',
+      `SlackWebhookClient: do not allow constructing client with ${config} string. Use object instead.`
+    );
+
+    this.onRequest = config.onRequest;
+
+    // incoming webhooks
+    // https://api.slack.com/incoming-webhooks
+    this.axios = axios.create({
+      baseURL: config.url,
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    this.axios.interceptors.request.use(
+      createRequestInterceptor({ onRequest: this.onRequest })
+    );
+  }
+
+  /**
+   * Send message by using raw body.
+   *
+   * @param body - Raw data to be sent.
+   *
+   * @see https://api.slack.com/docs/messages
+   *
+   * @example
+   *
+   * ```js
+   * await client.sendRawBody({ text: 'Hello!' });
+   * ```
+   */
+  sendRawBody(
+    body: Record<string, any>
+  ): Promise<SlackTypes.SendMessageSuccessResponse> {
+    return this.axios.post('', snakecaseKeysDeep(body)).then((res) => res.data);
+  }
+
+  /**
+   * Send text message.
+   *
+   * @param text - Text of the message to be sent.
+   *
+   * @see https://api.slack.com/docs/messages
+   *
+   * @example
+   *
+   * ```js
+   * await client.sendText('Hello!');
+   * ```
+   */
+  sendText(text: string): Promise<SlackTypes.SendMessageSuccessResponse> {
+    return this.sendRawBody({ text });
+  }
+
+  /**
+   * Send multiple attachments which let you add more context to a message.
+   *
+   * @param attachments
