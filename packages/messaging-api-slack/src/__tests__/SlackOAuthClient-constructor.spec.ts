6de5f148b12f8b53dd010f69d1d8218df568c280
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2021-12-12 16:44:59 +0000

    remove debug info

--- packages/messaging-api-slack/src/__tests__/SlackOAuthClient-constructor.spec.ts
+++ packages/messaging-api-slack/src/__tests__/SlackOAuthClient-constructor.spec.ts
@@ -92,4 +92,52 @@ describe('constructor', () => {
         },
       },
     });
-    // eslint-disable-next-line no-ne
+    // eslint-disable-next-line no-new
+    new SlackOAuthClient({
+      accessToken: TOKEN,
+      origin: 'https://mydummytestserver.com',
+    });
+
+    expect(axios.create).toBeCalledWith({
+      baseURL: 'https://mydummytestserver.com/api/',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    });
+  });
+});
+
+describe('#axios', () => {
+  it('should return underlying http client', () => {
+    const client = new SlackOAuthClient({ accessToken: TOKEN });
+
+    expect(client.axios.get).toBeDefined();
+    expect(client.axios.post).toBeDefined();
+    expect(client.axios.put).toBeDefined();
+    expect(client.axios.delete).toBeDefined();
+  });
+});
+
+describe('#accessToken', () => {
+  it('should return underlying access token', () => {
+    const client = new SlackOAuthClient({ accessToken: TOKEN });
+
+    expect(client.accessToken).toBe(TOKEN);
+  });
+});
+
+describe('#onRequest', () => {
+  it('should call onRequest when calling any API', async () => {
+    const onRequest = jest.fn();
+    const client = new SlackOAuthClient({
+      accessToken: TOKEN,
+      onRequest,
+    });
+
+    const mock = new MockAdapter(client.axios);
+
+    mock.onPost('/path').reply(200, {});
+
+    await client.axios.post('/path', { x: 1 });
+
+    expect(onRequest).toBeCalledWith({
+      method: 'post',
+      url: 'https://slack.com/
