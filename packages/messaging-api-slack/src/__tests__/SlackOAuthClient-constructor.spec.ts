3ce4e47f61586b32c75f02b47ab82f8c1cbeefb3
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2021-12-12 10:13:06 +0000

    chore: update common templates

--- packages/messaging-api-slack/src/__tests__/SlackOAuthClient-constructor.spec.ts
+++ packages/messaging-api-slack/src/__tests__/SlackOAuthClient-constructor.spec.ts
@@ -38,4 +38,58 @@ describe('connect', () => {
     axios.create = jest.fn().mockReturnValue({
       interceptors: {
         request: {
-          use: 
+          use: jest.fn(),
+        },
+      },
+    });
+    SlackOAuthClient.connect({
+      accessToken: TOKEN,
+      origin: 'https://mydummytestserver.com',
+    });
+
+    expect(axios.create).toBeCalledWith({
+      baseURL: 'https://mydummytestserver.com/api/',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    });
+  });
+});
+
+describe('constructor', () => {
+  let axios;
+  let _create;
+  beforeEach(() => {
+    axios = require('axios');
+    _create = axios.create;
+  });
+
+  afterEach(() => {
+    axios.create = _create;
+  });
+
+  describe('create axios with with slack api url', () => {
+    it('with config', () => {
+      axios.create = jest.fn().mockReturnValue({
+        interceptors: {
+          request: {
+            use: jest.fn(),
+          },
+        },
+      });
+      new SlackOAuthClient({ accessToken: TOKEN }); // eslint-disable-line no-new
+
+      expect(axios.create).toBeCalledWith({
+        baseURL: 'https://slack.com/api/',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      });
+    });
+  });
+
+  it('support origin', () => {
+    axios.create = jest.fn().mockReturnValue({
+      interceptors: {
+        request: {
+          use: jest.fn(),
+        },
+      },
+    });
+    // eslint-disable-next-line no-ne
