7228257b5a75a6c4a1c64194783e106c650fca16
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2022-01-02 19:40:47 +0000

    Update codesnippets.md

--- packages/messaging-api-slack/src/__tests__/SlackOAuthClient.spec.ts
+++ packages/messaging-api-slack/src/__tests__/SlackOAuthClient.spec.ts
@@ -546,4 +546,37 @@ describe('#chat.postEphemeral', () => {
     expect(res).toEqual(reply);
   });
 
-  it('should call chat.postEphemeral with channel, user and text and attachment
+  it('should call chat.postEphemeral with channel, user and text and attachments message', async () => {
+    const { client, mock } = createMock();
+
+    const reply = {
+      ok: true,
+      ts: '1405895017.000506',
+      channel: 'C024BE91L',
+      message: {},
+    };
+
+    mock
+      .onPost(
+        '/chat.postEphemeral',
+        querystring.stringify({
+          channel: CHANNEL,
+          user: USER,
+          text: 'hello',
+          attachments:
+            '[{"text":"Choose a game to play","fallback":"You are unable to choose a game","callback_id":"wopr_game","color":"#3AA3E3","attachment_type":"default","actions":[{"name":"game","text":"Chess","type":"button","value":"chess"},{"name":"game","text":"Falken\'s Maze","type":"button","value":"maze"},{"name":"game","text":"Thermonuclear War","style":"danger","type":"button","value":"war","confirm":{"title":"Are you sure?","text":"Wouldn\'t you prefer a good game of chess?","ok_text":"Yes","dismiss_text":"No"}}]}]',
+          as_user: true,
+          token: TOKEN,
+        }),
+        {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/x-www-form-urlencoded',
+        }
+      )
+      .reply(200, reply);
+
+    const res = await client.chat.postEphemeral({
+      channel: CHANNEL,
+      user: USER,
+      text: 'hello',
+  
