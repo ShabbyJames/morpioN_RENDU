f393baf6d8e04104219f4c5c7c0318b683638e4e
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2022-01-16 20:01:26 +0000

    GIT_SILENT: increase version

--- packages/messaging-api-slack/src/__tests__/SlackOAuthClient.spec.ts
+++ packages/messaging-api-slack/src/__tests__/SlackOAuthClient.spec.ts
@@ -827,4 +827,39 @@ describe('#chat.update', () => {
         querystring.stringify({
           channel: CHANNEL,
           attachments:
-            '[{"text":"Choose a game to play","fallback":"You are unable to choose a game","callback_id":"wopr_game","color":"#3AA3E3","attachment_type":"default","actions":[{"name":"game"
+            '[{"text":"Choose a game to play","fallback":"You are unable to choose a game","callback_id":"wopr_game","color":"#3AA3E3","attachment_type":"default","actions":[{"name":"game","text":"Chess","type":"button","value":"chess"},{"name":"game","text":"Falken\'s Maze","type":"button","value":"maze"},{"name":"game","text":"Thermonuclear War","style":"danger","type":"button","value":"war","confirm":{"title":"Are you sure?","text":"Wouldn\'t you prefer a good game of chess?","ok_text":"Yes","dismiss_text":"No"}}]}]',
+          as_user: true,
+          ts: '1405894322.332768',
+          token: TOKEN,
+        }),
+        {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/x-www-form-urlencoded',
+        }
+      )
+      .reply(200, reply);
+
+    const res = await client.chat.update({
+      channel: CHANNEL,
+      attachments: [
+        {
+          text: 'Choose a game to play',
+          fallback: 'You are unable to choose a game',
+          callbackId: 'wopr_game',
+          color: '#3AA3E3',
+          attachmentType: 'default',
+          actions: [
+            {
+              name: 'game',
+              text: 'Chess',
+              type: 'button',
+              value: 'chess',
+            },
+            {
+              name: 'game',
+              text: "Falken's Maze",
+              type: 'button',
+              value: 'maze',
+            },
+            {
+       
