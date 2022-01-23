bc4933f6f8e023613ece137d910b1fde49a2a3a0
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2022-01-23 21:48:51 +0000

    Version bump to 0.0.18

--- packages/messaging-api-slack/src/__tests__/SlackOAuthClient.spec.ts
+++ packages/messaging-api-slack/src/__tests__/SlackOAuthClient.spec.ts
@@ -1119,4 +1119,52 @@ describe('#chat.scheduleMessage', () => {
           channel: CHANNEL,
           attachments:
             '[{"text":"Choose a game to play","fallback":"You are unable to choose a game","callback_id":"wopr_game","color":"#3AA3E3","attachment_type":"default","actions":[{"name":"game","text":"Chess","type":"button","value":"chess"},{"name":"game","text":"Falken\'s Maze","type":"button","value":"maze"},{"name":"game","text":"Thermonuclear War","style":"danger","type":"button","value":"war","confirm":{"title":"Are you sure?","text":"Wouldn\'t you prefer a good game of chess?","ok_text":"Yes","dismiss_text":"No"}}]}]',
-        
+          as_user: true,
+          post_at: '299876400',
+          token: TOKEN,
+        }),
+        {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/x-www-form-urlencoded',
+        }
+      )
+      .reply(200, reply);
+
+    const res = await client.chat.scheduleMessage({
+      channel: CHANNEL,
+      attachments: [
+        {
+          text: 'Choose a game to play',
+          fallback: 'You are unable to choose a game',
+          callbackId: 'wopr_game',
+          color: '#3AA3E3',
+          attachmentType: 'default',
+          actions: [
+            {
+              name: 'game',
+              text: 'Chess',
+              type: 'button',
+              value: 'chess',
+            },
+            {
+              name: 'game',
+              text: "Falken's Maze",
+              type: 'button',
+              value: 'maze',
+            },
+            {
+              name: 'game',
+              text: 'Thermonuclear War',
+              style: 'danger',
+              type: 'button',
+              value: 'war',
+              confirm: {
+                title: 'Are you sure?',
+                text: "Wouldn't you prefer a good game of chess?",
+                okText: 'Yes',
+                dismissText: 'No',
+              },
+            },
+          ],
+        },
+     
