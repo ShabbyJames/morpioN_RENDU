4a25847386b2782ea5a3cd8e47362253eef2bf49
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2022-03-05 15:02:35 +0000

    demo code

--- packages/messaging-api-telegram/src/__tests__/TelegramClient-group.spec.ts
+++ packages/messaging-api-telegram/src/__tests__/TelegramClient-group.spec.ts
@@ -20,4 +20,54 @@ describe('group api', () => {
       result,
     };
 
-    it('should kick chat member with sn
+    it('should kick chat member with snakecase', async () => {
+      const { client, mock } = createMock();
+      mock
+        .onPost('/kickChatMember', {
+          chat_id: 427770117,
+          user_id: 313534466,
+          until_date: 1502855973,
+        })
+        .reply(200, reply);
+
+      const res = await client.kickChatMember(427770117, 313534466, {
+        until_date: 1502855973,
+      });
+      expect(res).toEqual(result);
+    });
+
+    it('should kick chat member with camelcase', async () => {
+      const { client, mock } = createMock();
+      mock
+        .onPost('/kickChatMember', {
+          chat_id: 427770117,
+          user_id: 313534466,
+          until_date: 1502855973,
+        })
+        .reply(200, reply);
+
+      const res = await client.kickChatMember(427770117, 313534466, {
+        untilDate: 1502855973,
+      });
+      expect(res).toEqual(result);
+    });
+  });
+
+  describe('#unbanChatMember', () => {
+    it('should unban chat member', async () => {
+      const { client, mock } = createMock();
+      const result = true;
+      const reply = {
+        ok: true,
+        result,
+      };
+
+      mock
+        .onPost('/unbanChatMember', {
+          chat_id: 427770117,
+          user_id: 313534466,
+        })
+        .reply(200, reply);
+
+      const res = await client.unbanChatMember(427770117, 313534466);
+      expect(res).toEqual
