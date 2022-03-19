c7ed878d8836a358db201c92b00f1d2dd86c2fa4
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2022-03-19 20:43:45 +0000

    Updated Portuguese translation and converted to New Spelling (Novo AO)

--- packages/messaging-api-telegram/src/__tests__/TelegramClient-stickerSet.spec.ts
+++ packages/messaging-api-telegram/src/__tests__/TelegramClient-stickerSet.spec.ts
@@ -9,4 +9,50 @@ const createMock = (): { client: TelegramClient; mock: MockAdapter } => {
     accessToken: ACCESS_TOKEN,
   });
   const mock = new MockAdapter(client.axios);
-  return 
+  return { client, mock };
+};
+
+describe('sticker set api', () => {
+  describe('#getStickerSet', () => {
+    it('should return a stickerSet', async () => {
+      const { client, mock } = createMock();
+      const result = {
+        name: 'sticker set name',
+        title: 'sticker set title',
+        isAnimated: false,
+        containsMasks: false,
+        stickers: [
+          {
+            width: 512,
+            height: 512,
+            emoji: '💛',
+            setName: 'sticker set name',
+            isAnimated: false,
+            thumb: {
+              fileId: 'AAQEAANDAQACEDVoAAFVA7aGNPt1If3eYTAABAEAB20AAzkOAAIWB',
+              fileSize: 5706,
+              width: 128,
+              height: 128,
+            },
+            fileId: 'CAADBAADQwEAAhA1aAABVQO2hjT7dSEWB',
+            fileSize: 36424,
+          },
+        ],
+      };
+      const reply = {
+        ok: true,
+        result: {
+          name: 'sticker set name',
+          title: 'sticker set title',
+          is_animated: false,
+          contains_masks: false,
+          stickers: [
+            {
+              width: 512,
+              height: 512,
+              emoji: '💛',
+              set_name: 'sticker set name',
+              is_animated: false,
+              thumb: {
+                file_id:
+                
