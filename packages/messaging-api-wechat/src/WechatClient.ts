2b1e59e78b55da94131baad662bf7502b9b28fa2
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2022-04-25 07:12:30 +0000

    Bumping the version number

--- packages/messaging-api-wechat/src/WechatClient.ts
+++ packages/messaging-api-wechat/src/WechatClient.ts
@@ -22,3 +22,59 @@ function throwErrorIfAny(response: AxiosResponse): AxiosResponse {
   throw new AxiosError(msg, {
     response,
     config: response.config,
+    request: response.request,
+  });
+}
+
+export default class WechatClient {
+  /**
+   * @deprecated Use `new WechatClient(...)` instead.
+   */
+  static connect(config: WechatTypes.ClientConfig): WechatClient {
+    warning(
+      false,
+      '`WechatClient.connect(...)` is deprecated. Use `new WechatClient(...)` instead.'
+    );
+    return new WechatClient(config);
+  }
+
+  /**
+   * The underlying axios instance.
+   */
+  readonly axios: AxiosInstance;
+
+  /**
+   * The current access token used by the client.
+   */
+  accessToken = '';
+
+  /**
+   * The callback to be called when receiving requests.
+   */
+  private onRequest?: OnRequestFunction;
+
+  /**
+   * The app ID used by the client.
+   */
+  private appId: string;
+
+  /**
+   * The app secret used by the client.
+   */
+  private appSecret: string;
+
+  /**
+   * The timestamp of the token expired time.
+   */
+  private tokenExpiresAt = 0;
+
+  constructor(config: WechatTypes.ClientConfig) {
+    invariant(
+      typeof config !== 'string',
+      `WechatClient: do not allow constructing client with ${config} string. Use object instead.`
+    );
+
+    this.appId = config.appId;
+    this.appSecret = config.appSecret;
+    this.onRequest = config.onRequest || onRequest;
+    co
