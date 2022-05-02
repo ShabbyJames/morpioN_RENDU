565240e715128d5d0844a13fbd877ba608123130
ShabbyJames <91358563+ShabbyJames@users.noreply.github.com>
2022-05-02 11:27:59 +0000

    npm update

--- packages/messaging-api-wechat/src/__tests__/WechatClient.spec.ts
+++ packages/messaging-api-wechat/src/__tests__/WechatClient.spec.ts
@@ -273,3 +273,338 @@ describe('send api', () => {
 
   describe('#sendMusic', () => {
     it('should call wechat api', async () => {
+      const { client, mock } = createMock();
+
+      const reply = {
+        errcode: 0,
+        errmsg: 'ok',
+      };
+
+      mock
+        .onPost(`/message/custom/send?access_token=${ACCESS_TOKEN}`, {
+          touser: RECIPIENT_ID,
+          msgtype: 'music',
+          music: {
+            title: 'MUSIC_TITLE',
+            description: 'MUSIC_DESCRIPTION',
+            musicurl: 'MUSIC_URL',
+            hqmusicurl: 'HQ_MUSIC_URL',
+            thumb_media_id: 'THUMB_MEDIA_ID',
+          },
+        })
+        .reply(200, reply);
+
+      const res = await client.sendMusic(RECIPIENT_ID, {
+        title: 'MUSIC_TITLE',
+        description: 'MUSIC_DESCRIPTION',
+        musicurl: 'MUSIC_URL',
+        hqmusicurl: 'HQ_MUSIC_URL',
+        thumbMediaId: 'THUMB_MEDIA_ID',
+      });
+
+      expect(res).toEqual(reply);
+    });
+
+    it('should support snakecase', async () => {
+      const { client, mock } = createMock();
+
+      const reply = {
+        errcode: 0,
+        errmsg: 'ok',
+      };
+
+      mock
+        .onPost(`/message/custom/send?access_token=${ACCESS_TOKEN}`, {
+          touser: RECIPIENT_ID,
+          msgtype: 'music',
+          music: {
+            title: 'MUSIC_TITLE',
+            description: 'MUSIC_DESCRIPTION',
+            musicurl: 'MUSIC_URL',
+            hqmusicurl: 'HQ_MUSIC_URL',
+            thumb_media_id: 'THUMB_MEDIA_ID',
+          },
+        })
+        .reply(200, reply);
+
+      const res = await client.sendMusic(RECIPIENT_ID, {
+        title: 'MUSIC_TITLE',
+        description: 'MUSIC_DESCRIPTION',
+        musicurl: 'MUSIC_URL',
+        hqmusicurl: 'HQ_MUSIC_URL',
+        // @ts-expect-error
+        thumb_media_id: 'THUMB_MEDIA_ID',
+      });
+
+      expect(res).toEqual(reply);
+    });
+  });
+
+  describe('#sendNews', () => {
+    it('should call wechat api', async () => {
+      const { client, mock } = createMock();
+
+      const reply = {
+        errcode: 0,
+        errmsg: 'ok',
+      };
+
+      mock
+        .onPost(`/message/custom/send?access_token=${ACCESS_TOKEN}`, {
+          touser: RECIPIENT_ID,
+          msgtype: 'news',
+          news: {
+            articles: [
+              {
+                title: 'Happy Day',
+                description: 'Is Really A Happy Day',
+                url: 'URL',
+                picurl: 'PIC_URL',
+              },
+              {
+                title: 'Happy Day',
+                description: 'Is Really A Happy Day',
+                url: 'URL',
+                picurl: 'PIC_URL',
+              },
+            ],
+          },
+        })
+        .reply(200, reply);
+
+      const res = await client.sendNews(RECIPIENT_ID, {
+        articles: [
+          {
+            title: 'Happy Day',
+            description: 'Is Really A Happy Day',
+            url: 'URL',
+            picurl: 'PIC_URL',
+          },
+          {
+            title: 'Happy Day',
+            description: 'Is Really A Happy Day',
+            url: 'URL',
+            picurl: 'PIC_URL',
+          },
+        ],
+      });
+
+      expect(res).toEqual(reply);
+    });
+  });
+
+  describe('#sendMPNews', () => {
+    it('should call wechat api', async () => {
+      const { client, mock } = createMock();
+
+      const reply = {
+        errcode: 0,
+        errmsg: 'ok',
+      };
+
+      mock
+        .onPost(`/message/custom/send?access_token=${ACCESS_TOKEN}`, {
+          touser: RECIPIENT_ID,
+          msgtype: 'mpnews',
+          mpnews: {
+            media_id: 'MEDIA_ID',
+          },
+        })
+        .reply(200, reply);
+
+      const res = await client.sendMPNews(RECIPIENT_ID, 'MEDIA_ID');
+
+      expect(res).toEqual(reply);
+    });
+  });
+
+  describe('#sendMsgMenu', () => {
+    it('should call wechat api', async () => {
+      const { client, mock } = createMock();
+
+      const reply = {
+        errcode: 0,
+        errmsg: 'ok',
+      };
+
+      mock
+        .onPost(`/message/custom/send?access_token=${ACCESS_TOKEN}`, {
+          touser: RECIPIENT_ID,
+          msgtype: 'msgmenu',
+          msgmenu: {
+            head_content: 'HEAD',
+            list: [
+              {
+                id: '101',
+                content: 'Yes',
+              },
+              {
+                id: '102',
+                content: 'No',
+              },
+            ],
+            tail_content: 'TAIL',
+          },
+        })
+        .reply(200, reply);
+
+      const res = await client.sendMsgMenu(RECIPIENT_ID, {
+        headContent: 'HEAD',
+        list: [
+          {
+            id: '101',
+            content: 'Yes',
+          },
+          {
+            id: '102',
+            content: 'No',
+          },
+        ],
+        tailContent: 'TAIL',
+      });
+
+      expect(res).toEqual(reply);
+    });
+
+    it('should support snakecase', async () => {
+      const { client, mock } = createMock();
+
+      const reply = {
+        errcode: 0,
+        errmsg: 'ok',
+      };
+
+      mock
+        .onPost(`/message/custom/send?access_token=${ACCESS_TOKEN}`, {
+          touser: RECIPIENT_ID,
+          msgtype: 'msgmenu',
+          msgmenu: {
+            head_content: 'HEAD',
+            list: [
+              {
+                id: '101',
+                content: 'Yes',
+              },
+              {
+                id: '102',
+                content: 'No',
+              },
+            ],
+            tail_content: 'TAIL',
+          },
+        })
+        .reply(200, reply);
+
+      const res = await client.sendMsgMenu(RECIPIENT_ID, {
+        // @ts-expect-error
+        head_content: 'HEAD',
+        list: [
+          {
+            id: '101',
+            content: 'Yes',
+          },
+          {
+            id: '102',
+            content: 'No',
+          },
+        ],
+        tail_content: 'TAIL',
+      });
+
+      expect(res).toEqual(reply);
+    });
+  });
+
+  describe('#sendWXCard', () => {
+    it('should call wechat api', async () => {
+      const { client, mock } = createMock();
+
+      const reply = {
+        errcode: 0,
+        errmsg: 'ok',
+      };
+
+      mock
+        .onPost(`/message/custom/send?access_token=${ACCESS_TOKEN}`, {
+          touser: RECIPIENT_ID,
+          msgtype: 'wxcard',
+          wxcard: {
+            card_id: '123dsdajkasd231jhksad',
+          },
+        })
+        .reply(200, reply);
+
+      const res = await client.sendWXCard(
+        RECIPIENT_ID,
+        '123dsdajkasd231jhksad'
+      );
+
+      expect(res).toEqual(reply);
+    });
+  });
+
+  describe('#sendMiniProgramPage', () => {
+    it('should call wechat api', async () => {
+      const { client, mock } = createMock();
+
+      const reply = {
+        errcode: 0,
+        errmsg: 'ok',
+      };
+
+      mock
+        .onPost(`/message/custom/send?access_token=${ACCESS_TOKEN}`, {
+          touser: RECIPIENT_ID,
+          msgtype: 'miniprogrampage',
+          miniprogrampage: {
+            title: 'title',
+            appid: 'appid',
+            pagepath: 'pagepath',
+            thumb_media_id: 'thumb_media_id',
+          },
+        })
+        .reply(200, reply);
+
+      const res = await client.sendMiniProgramPage(RECIPIENT_ID, {
+        title: 'title',
+        appid: 'appid',
+        pagepath: 'pagepath',
+        thumbMediaId: 'thumb_media_id',
+      });
+
+      expect(res).toEqual(reply);
+    });
+
+    it('should support snakecase', async () => {
+      const { client, mock } = createMock();
+
+      const reply = {
+        errcode: 0,
+        errmsg: 'ok',
+      };
+
+      mock
+        .onPost(`/message/custom/send?access_token=${ACCESS_TOKEN}`, {
+          touser: RECIPIENT_ID,
+          msgtype: 'miniprogrampage',
+          miniprogrampage: {
+            title: 'title',
+            appid: 'appid',
+            pagepath: 'pagepath',
+            thumb_media_id: 'thumb_media_id',
+          },
+        })
+        .reply(200, reply);
+
+      const res = await client.sendMiniProgramPage(RECIPIENT_ID, {
+        title: 'title',
+        appid: 'appid',
+        pagepath: 'pagepath',
+        // @ts-expect-error
+        thumb_media_id: 'thumb_media_id',
+      });
+
+      expect(res).toEqual(reply);
+    });
+  });
+});
